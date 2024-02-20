938bd2ff859cbfd3253193406e034fb827862065
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2024-02-20 14:13:04 -0300

    feat: html and js from the third test and change variables name

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const firstTestButton = document.getElementById("firstTestButton");
 
 firstTestButton.addEventListener("click", () => {
   const firstTestInput = document.getElementById("firstTestInput");
-  const endereco = firstTestInput.value.split(" ");
+  const address = firstTestInput.value.split(" ");
 
   const firstTestP = document.getElementById("firstTestP");
-  firstTestP.innerHTML = `{rua: ${endereco[0]}, numero: ${endereco[1]}}`;
+  firstTestP.innerHTML = `{rua: ${address[0]}, numero: ${address[1]}}`;
 });
 
 // 2 teste
@@ -14,20 +14,68 @@ const secondTestButton = document.getElementById("secondTestButton");
 
 secondTestButton.addEventListener("click", () => {
   const secondTestInput = document.getElementById("secondTestInput");
-  const arrayEndereco = secondTestInput.value.split(" ");
+  const arrayAddress = secondTestInput.value.split(" ");
 
-  let rua = "";
+  let street = "";
 
-  for (let i = 0; i < arrayEndereco.length; i++) {
-    if (!Number(arrayEndereco[i])) {
-      rua += `${arrayEndereco[i]} `;
+  for (let i = 0; i < arrayAddress.length; i++) {
+    if (!Number(arrayAddress[i])) {
+      street += `${arrayAddress[i]} `;
     } else {
       break;
     }
   }
 
   const secondTestP = document.getElementById("secondTestP");
-  secondTestP.innerHTML = `{rua: ${rua.trim()}, numero: ${arrayEndereco
-    .slice(rua.split(" ").length - 1)
+  secondTestP.innerHTML = `{rua: ${street.trim()}, numero: ${arrayAddress
+    .slice(street.split(" ").length - 1)
     .join(" ")}}`;
+});
+
+// 3 teste
+const separateAddress = (address) => {
+  let arrayAddress = address.split(", ");
+
+  if (arrayAddress.length > 1 && Number(arrayAddress[0])) {
+    return { street: arrayAddress[1], number: arrayAddress[0] };
+  } else if (arrayAddress.length > 1) {
+    return { street: arrayAddress[0], number: arrayAddress[1] };
+  }
+
+  arrayAddress = address.split(" No");
+
+  if (arrayAddress.length > 1) {
+    return { street: arrayAddress[0], number: `No${arrayAddress[1]}` };
+  }
+
+  arrayAddress = address.split(" ");
+
+  if (Number(arrayAddress[0])) {
+    return { street: arrayAddress.slice(1).join(" "), number: arrayAddress[0] };
+  } else {
+    let street = "";
+
+    for (let i = 0; i < arrayAddress.length; i++) {
+      if (!Number(arrayAddress[i])) {
+        street += `${arrayAddress[i]} `;
+      } else {
+        break;
+      }
+    }
+
+    return {
+      street: street.trim(),
+      number: arrayAddress.slice(street.split(" ").length - 1).join(" "),
+    };
+  }
+};
+
+const thirdTestButton = document.getElementById("thirdTestButton");
+
+thirdTestButton.addEventListener("click", () => {
+  const thirdTestInput = document.getElementById("thirdTestInput");
+  const address = separateAddress(thirdTestInput.value);
+
+  const thirdTestP = document.getElementById("thirdTestP");
+  thirdTestP.innerHTML = `{rua: ${address.street}, numero: ${address.number}}`;
 });
