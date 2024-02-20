d29473d85b7f5dda15b08a9e0ecadd46dd5d8299
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2024-02-20 13:37:05 -0300

    feat: html and js from the second test

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ firstTestButton.addEventListener("click", () => {
   const firstTestP = document.getElementById("firstTestP");
   firstTestP.innerHTML = `{rua: ${endereco[0]}, numero: ${endereco[1]}}`;
 });
+
+// 2 teste
+const secondTestButton = document.getElementById("secondTestButton");
+
+secondTestButton.addEventListener("click", () => {
+  const secondTestInput = document.getElementById("secondTestInput");
+  const arrayEndereco = secondTestInput.value.split(" ");
+
+  let rua = "";
+
+  for (let i = 0; i < arrayEndereco.length; i++) {
+    if (!Number(arrayEndereco[i])) {
+      rua += `${arrayEndereco[i]} `;
+    } else {
+      break;
+    }
+  }
+
+  const secondTestP = document.getElementById("secondTestP");
+  secondTestP.innerHTML = `{rua: ${rua.trim()}, numero: ${arrayEndereco
+    .slice(rua.split(" ").length - 1)
+    .join(" ")}}`;
+});
